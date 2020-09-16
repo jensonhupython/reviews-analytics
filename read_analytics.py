79f65c02861b6292abf3063f7d4f9e7d1bb5aa82
Jenson Hu <jenson7899@gmail.com>
2020-09-16 08:18:25 +0800

    [Python] To calculat the avg length of reviews

--- read_analytics.py
+++ read_analytics.py
@@ -8,7 +8,12 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 1000 == 0:  # "%" 求餘數
             print(len(data))
+print('檔案讀取完畢, 總共有', len(data), '筆資料')
 
-
+#計算每則留言的平均長度
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print('留言的平均長度為:', sum_len/len(data))    
 # Read the first data 
-print(data[0])
+#print(data[0])
