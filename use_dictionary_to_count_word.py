5b3af54a9a7d353b683e55b0376c9823fd465c0d
Jenson Hu <jenson7899@gmail.com>
2020-12-02 08:28:26 +0800

    [Python] add a feature that use dictionary to count specific word

---
+++ use_dictionary_to_count_word.py
@@ -0,0 +1,44 @@
+# 利用 dictionary 做一個功能
+# 功能: 可以計數某個字出現在整個留言中, 可以讓使用者輸入
+
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+    for line in f:
+        data.append(line)
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
+
+#print('檔案讀取完畢, 總共有', len(data), '筆資料')
+print(data[0])
+
+word_count = {} 
+for each_msg in data:
+    words = each_msg.split(' ')
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1  #新增新的key進字典
+    print(words)
+    break
+
+for key in word_count:
+    #print(key, word_count[key])
+    if word_count[key] > 2:
+        print(key, word_count[key])
+#print(word_count)
+
+print('歡迎使用查找功能, 離開請輸入 q')
+while True:
+    user_word = input('請問你想查什麼字: ')
+    if user_word == 'q':
+        break;
+    else:
+        if user_word in word_count:
+            print(user_word, '出現過的次數為: ', word_count[user_word])
+        else: 
+            print(user_word, '沒有出現過!!')
+
+print('感謝使用!!')        
